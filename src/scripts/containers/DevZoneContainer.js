45e6ff58306e1c389f92ac20067036a20aee4614
Lutando Ngqakaza <lutando@ngqakaza.com>
2017-07-23 18:50:23 +0200

    postroll2

---
+++ src/scripts/containers/DevZoneContainer.js
@@ -0,0 +1,63 @@
+import React, { PropTypes } from 'react'
+import { Row, Grid, Col } from 'react-bootstrap'
+
+class DevZoneContainer extends React.Component {
+
+    render() {
+        return (
+            <div className='container'>
+                <Row>
+
+                </Row>
+            </div>
+        )
+    }
+}
+
+{/*<!--
+
+                <div class="col col-sm-2 col-xs-1">
+                    <img src="assets/img/album-cover-placeholder.png" alt="Album Cover" class="album-cover">
+                    <p class="title">Say my name</p>
+                    <p class="time">(5:07)</p>
+                    <p class="artist">HaydenPa <span class="rep green">+950</span></p>
+                </div>
+                <div class="col col-sm-10 col-xs-11">
+
+                    <!-- Comment -->
+                    <div class="content">
+                        <div class="pull-left comment-score">
+                            <div class="vote">
+                                <button class="btn-vote transparent"><img src="assets/img/upvote-inactive.png" alt="Upvote *inactive"></button>
+                                <button class="btn-vote transparent"><img src="assets/img/downvote-inactive.png" alt="Downvote *inactive"></button>
+                            </div>
+                            <div class="current-score text-center">
+                                <p>243</p>
+                            </div>
+                        </div>
+
+                        <div class="comment">
+                            <div class="profile">
+                                <img src="assets/img/avatar-placeholder.png" class="img-circle img-profile">Username <span class="rep green">+950</span>
+
+                                <div class="pull-right"><a href="#" class="btn-reply">Reply</a></div>
+                            </div>
+
+                            <div class="entered-comment">
+                                <p>Really nice! I love the moment at <a href="#">@11:38</a>! Maybe add some more bass, and then it should be perfect IMO. Great job, looking forward to your new works!</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="parent-options">
+                        <a href="comments.html" class="btn-read-more pull-right">Read 17 more comments</a>
+                    </div>
+
+                    <div class="enter-comment">
+                        <!-- new parent comment -->
+                        <textarea class="form-control" rows="3" style=""></textarea>
+                        <button class="btn btn-primary">Post comment</button>
+                    </div>
+                </div>
+-->
+*/}
